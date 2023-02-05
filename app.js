aab709cb5a7c61c7f782cd423da9bfbade26da18
anurag-int <85058448+anurag-int@users.noreply.github.com>
2023-02-05 22:24:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-// array that is used to store the todo list items
+
 var items = [];
 
-// always need to set the view engine when use ejs
+
 app.set('view engine', 'ejs');
 
-// always need to write when we use the body-parser
+
 app.use(bodyParser.urlencoded({extended: true}));
 
-// it is to make use public as a static resource
+
 app.use(express.static("public"));
 
 app.post("/", function(req, res){
@@ -31,14 +31,13 @@ app.get("/", (req, res)=>{
     
     var today = new Date();
     
-    // to get date in (September 13, 2022) this format
+   
     var dateDescription = {
         weekday : "long",
         day : "numeric",
         month : "long"
     };
 
-    // to get date in (September 13, 2022) this format
     var day = today.toLocaleDateString("en-US", dateDescription);
     
     res.render("list", {listTitle : day, newListItem: items});
